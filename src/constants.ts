97bd8cdb18b20ddea2c779adbad3e93b8206b80e
Lucas <lluc23@hotmail.com>
2023-07-25 18:58:51 +0200

    set dynamics envs

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,6 @@
-let PUBLIC_BACK_DOMAIN;
-try {
-    const module = await import('$env/static/public');
-    PUBLIC_BACK_DOMAIN = module.PUBLIC_BACK_DOMAIN;
+import { env } from '$env/dynamic/public';
 
-} catch {
-    PUBLIC_BACK_DOMAIN = "https://api.elerem.com"
-}
-export const API_DOMAIN = PUBLIC_BACK_DOMAIN
+export const API_DOMAIN = env.PUBLIC_BACK_DOMAIN
 export const CURRENT_VERSION = "v1"
 export const API_URL = `${API_DOMAIN}/api/${CURRENT_VERSION}/recommendations/`
 export const ASSETS_URL = `${API_DOMAIN}/assets/embed-widget.js`
